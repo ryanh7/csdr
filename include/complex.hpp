124a3a84b043af5f2683fb3bda78a086e5e6e5a2
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-07-07 17:24:58 +0200

    rename variables

--- include/complex.hpp
+++ include/complex.hpp
@@ -7,7 +7,7 @@ namespace Csdr {
     template <typename T>
     class complex: public std::complex<T> {
         public:
-            complex(const T& re = T(), const T& im = T()): std::complex<T>(re, im) {}
+            complex(const T& i = T(), const T& q = T()): std::complex<T>(i, q) {}
             complex(std::complex<T> c): complex(c.real(), c.imag()) {}
             // in-phase
             T i() { return std::complex<T>::real(); }
