e5248a479b231a06a2f1fbff5c0a7e8c732f9e86
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-07-20 17:53:49 +0200

    introduce untyped agc type

--- include/agc.hpp
+++ include/agc.hpp
@@ -6,17 +6,27 @@
 
 namespace Csdr {
 
+    class UntypedAgc {
+        public:
+            virtual void setReference(float reference) = 0;
+            virtual void setAttack(float attack_rate) = 0;
+            virtual void setDecay(float decay_rate) = 0;
+            virtual void setMaxGain(float max_gain) = 0;
+            virtual void setInitialGain(float initial_gain) = 0;
+            virtual void setHangTime(unsigned long int hang_time) = 0;
+    };
+
     template <typename T>
-    class Agc: public AnyLengthModule<T, T> {
+    class Agc: public UntypedAgc, public AnyLengthModule<T, T> {
         public:
             void process(T* input, T* output, size_t work_size) override;
 
-            void setReference(float reference);
-            void setAttack(float attack_rate);
-            void setDecay(float decay_rate);
-            void setMaxGain(float max_gain);
-            void setInitialGain(float initial_gain);
-            void setHangTime(unsigned long int hang_time);
+            void setReference(float reference) override;
+            void setAttack(float attack_rate) override;
+            void setDecay(float decay_rate) override;
+            void setMaxGain(float max_gain) override;
+            void setInitialGain(float initial_gain) override;
+            void setHangTime(unsigned long int hang_time) override;
         private:
             float abs(T in);
             bool isZero(T in);
