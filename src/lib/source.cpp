e975126879a6940fa2ab3bdf3aba96def904ccff
Jakob Ketterl <jakob.ketterl@gmx.de>
2021-07-24 18:37:42 +0200

    create additional source types because they may be referenced

--- src/lib/source.cpp
+++ src/lib/source.cpp
@@ -86,5 +86,8 @@ namespace Csdr {
     template class Source<complex<float>>;
     template class Source<unsigned char>;
 
+    template class TcpSource<unsigned char>;
+    template class TcpSource<short>;
+    template class TcpSource<float>;
     template class TcpSource<complex<float>>;
 }
