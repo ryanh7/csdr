e2bc5cd6c7eee19767e89a4929630a33b11aea8c
Jakob Ketterl <jakob.ketterl@gmx.de>
2023-06-30 13:10:04 +0200

    remove old csdr remnants; rename csdr++ -> csdr

--- src/apps/csdr/csdr.cpp
+++ src/apps/csdr/csdr.cpp
@@ -1,23 +1,23 @@
 /*
-Copyright (c) 2021 Jakob Ketterl <jakob.ketterl@gmx.de>
+Copyright (c) 2021-2023 Jakob Ketterl <jakob.ketterl@gmx.de>
 
-This file is part of csdr++.
+This file is part of csdr.
 
-csdr++ is free software: you can redistribute it and/or modify
+csdr is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-csdr++ is distributed in the hope that it will be useful,
+csdr is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with csdr++.  If not, see <https://www.gnu.org/licenses/>.
+along with csdr.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "csdr++.hpp"
+#include "csdr.hpp"
 #include "ringbuffer.hpp"
 #include "writer.hpp"
 #include "agc.hpp"
@@ -73,7 +73,7 @@ int Cli::main(int argc, char** argv) {
         app.parse(argc, argv);
     } catch (const CLI::ParseError &e) {
         if (*version_flag) {
-            std::cerr << "csdr++ version " << VERSION << "\n";
+            std::cerr << "csdr version " << VERSION << "\n";
             return 0;
         }
 
